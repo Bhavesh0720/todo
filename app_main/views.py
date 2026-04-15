a103f3167042af3d3f879df6a3dff315e3596a05
Bhavesh0720 <bhavesh0720@gmail.com>
2026-04-15 18:24:42 +0530

    retrived task dynamically and added add functionality

--- app_main/views.py
+++ app_main/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Task
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(is_completed=False)
+    completed_tasks = Task.objects.filter(is_completed=True)
+    con = {
+        'tasks':tasks,
+        'completed_tasks':completed_tasks,
+    }
+    return render(request, 'home.html', con)
+
+
+def addTask(request):
+    if request.method=='POST':
+        task = request.POST['task']
+        Task.objects.create(task=task)
+    return redirect('home')
