239480200ff497dc5cb15d02b4d5e80f2e9998f3
Bhavesh0720 <bhavesh0720@gmail.com>
2026-04-15 18:44:58 +0530

    added markAsDone functionality

--- app_main/urls.py
+++ app_main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add_task', views.addTask, name='addTask'),
+    path('mark_as_done/<int:pk>', views.markAsDone, name='markAsDone'),
 ]
--- app_main/views.py
+++ app_main/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
 # Create your views here.
@@ -17,4 +17,11 @@ def addTask(request):
     if request.method=='POST':
         task = request.POST['task']
         Task.objects.create(task=task)
+    return redirect('home')
+
+
+def markAsDone(request, pk):    
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
     return redirect('home')
